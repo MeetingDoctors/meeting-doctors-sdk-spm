cd993b2052e663d34176d608d35a89fd6859f7e1
Fabio Cuomo <fabio.cuomo@meetingdoctors.com>
2023-03-21 13:00:21 +0100

    chore: added meeting doctors code dependency

--- Package.swift
+++ Package.swift
@@ -4,25 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "meeting-doctors-sdk-spm",
+    name: "MeetingDoctorsSDK",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "meeting-doctors-sdk-spm",
-            targets: ["meeting-doctors-sdk-spm"]),
+            name: "MeetingDoctorsSDK",
+            targets: ["MeetingDoctorsSDK"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.6.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "meeting-doctors-sdk-spm",
-            dependencies: []),
-        .testTarget(
-            name: "meeting-doctors-sdk-spmTests",
-            dependencies: ["meeting-doctors-sdk-spm"]),
+            name: "MeetingDoctorsSDK",
+            dependencies: [
+                "MeetingDoctorsCore",
+                "CryptoSwift"
+            ]),
+        .binaryTarget(
+            name: "MeetingDoctorsCore",
+            path: "artifacts/MeetingDoctorsCore.xcframework"
+        )
     ]
 )
