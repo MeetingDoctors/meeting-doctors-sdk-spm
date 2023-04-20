91b4f21010c5d4f34cebf20ee5e7397eceb4308b
Fabio Cuomo <fabio.cuomo@meetingdoctors.com>
2023-04-20 15:09:28 +0200

    WIP

---
+++ Sources/FruitBasket/FruitBasket.swift
@@ -0,0 +1,10 @@
+import MeetingDoctorsCore
+
+public struct meeting_doctors_sdk_spm {
+    public private(set) var text = "Hello, World!"
+    public var type: BuildConfigurationType?
+    
+    public init() {
+        type = BuildConfigurationType.release
+    }
+}
